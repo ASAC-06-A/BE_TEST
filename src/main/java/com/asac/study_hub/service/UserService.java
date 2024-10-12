416a37ecfc3e960a7b70cf1a7f20167111d32e97
xxyeon <jykim9335@gmail.com>
2024-10-12 23:37:35 +0900

    Add: 더미 데이터 생성

--- src/main/java/com/asac/study_hub/service/UserService.java
+++ src/main/java/com/asac/study_hub/service/UserService.java
@@ -1,10 +1,13 @@
 package com.asac.study_hub.service;
 
+import com.asac.study_hub.domain.User;
 import com.asac.study_hub.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
@@ -12,5 +15,12 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     UserRepository userRepository;
+
+    private static final HashMap<Integer, User> users;
+    static {
+        users = new HashMap<>();
+        users.put(1, new User(1L, "김정현", "solee3020@gmail.com", "solee6810"));
+        users.put(2, new User(2L, "김지연", "jykim9335@gmail.com", "asas5656"));
+    }
 
 }
