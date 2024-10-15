f9f560da6069427cfd0b5c421289392621f092f5
xxyeon <jykim9335@gmail.com>
2024-10-15 16:37:14 +0900

    feat: UserRespository 인터페이스 CRUD

--- src/main/java/com/asac/study_hub/repository/UserRepository.java
+++ src/main/java/com/asac/study_hub/repository/UserRepository.java
@@ -1,8 +1,36 @@
 package com.asac.study_hub.repository;
 
+import com.asac.study_hub.domain.User;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class UserRepository {
+import java.util.List;
+import java.util.Optional;
 
+@Repository
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserRepository implements IRepository {
+    User user;
+
+    public User save(User user) {
+        return null;
+    }
+
+    public Optional<User> findById(Integer id) {
+        return Optional.empty();
+    }
+
+    public List<User> findAll() {
+        return List.of();
+    }
+
+    public void update(User user) {
+
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
 }
