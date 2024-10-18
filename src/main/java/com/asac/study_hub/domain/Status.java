86cf37d09caf25996028cb62196523982cd9a49e
xxyeon <jykim9335@gmail.com>
2024-10-18 15:20:44 +0900

    feat: soft deleted 를 위한 데이터 상태 정의

---
+++ src/main/java/com/asac/study_hub/domain/Status.java
@@ -0,0 +1,15 @@
+package com.asac.study_hub.domain;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public enum Status {
+    ACTIVE("활성화"),
+    INACTIVE("비활성화"),
+    DELETED("삭제된 데이터");
+
+    String status;
+}
