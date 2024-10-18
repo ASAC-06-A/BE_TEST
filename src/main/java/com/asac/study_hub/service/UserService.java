169fe1074b573a8e04215b9e0ff1e99c8ab615dd
xxyeon <jykim9335@gmail.com>
2024-10-18 23:37:26 +0900

    feat: 이메일, 닉네임 중복 예외 처리

--- src/main/java/com/asac/study_hub/service/UserService.java
+++ src/main/java/com/asac/study_hub/service/UserService.java
@@ -3,6 +3,8 @@ package com.asac.study_hub.service;
 import com.asac.study_hub.controller.dto.userDto.signupDto.SignupRequestDto;
 import com.asac.study_hub.controller.dto.userDto.signupDto.SignupResponseDto;
 import com.asac.study_hub.domain.User;
+import com.asac.study_hub.exception.CustomException;
+import com.asac.study_hub.exception.ExceptionType;
 import com.asac.study_hub.repository.UserRepository;
 import com.asac.study_hub.util.SessionProvider;
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,27 +24,46 @@ public class UserService {
     UserRepository userRepository;
 
     public SignupResponseDto signup(SignupRequestDto signupRequestDto) {
+        String email = signupRequestDto.getEmail();
+        String name = signupRequestDto.getUserName();
+
+        if (checkDuplicatedEmail(email)) {
+            throw new CustomException(ExceptionType.EXIST_EMAIL, email);
+        }
+        if (checkDuplicatedName(name)) {
+            throw new CustomException(ExceptionType.EXIST_NICKNAME, name);
+        }
+
         signupRequestDto.setId(userRepository.findAll().size() + 1);
         return SignupResponseDto.builder()
-                .userId(userRepository.save(SignupRequestDto.of(signupRequestDto)).getId())
-                .status(HttpStatus.CREATED.value())
-                .build();
+            .userId(userRepository.save(SignupRequestDto.of(signupRequestDto)).getId())
+            .status(HttpStatus.CREATED.value())
+            .build();
 
     }
 
-    public SignupResponseDto signin(HttpServletRequest request, HttpServletResponse response, SignupRequestDto userDto) {
+    public SignupResponseDto signin(HttpServletRequest request, HttpServletResponse response,
+        SignupRequestDto userDto) {
 
         saveSession(request, userDto);
 
         return SignupResponseDto.builder()
-                .userId(SignupRequestDto.of(userDto).getId())
-                .status(HttpStatus.OK.value())
-                .build();
+            .userId(SignupRequestDto.of(userDto).getId())
+            .status(HttpStatus.OK.value())
+            .build();
     }
 
     private void saveSession(HttpServletRequest request, SignupRequestDto userDto) {
         //로그인 한정 사용하는 메서드 따라서 세션이 있으면 그대로 사용하고 없으면 새로 생성하는 getSession 메서드 사용
         User user = userRepository.findByEmail(userDto.getEmail());
         SessionProvider.createSession(request, user);
+    }
+
+    private boolean checkDuplicatedEmail(String email) {
+        return userRepository.findAll().stream().noneMatch(user -> user.getEmail().equals(email));
+    }
+
+    private boolean checkDuplicatedName(String name) {
+        return userRepository.findAll().stream().noneMatch(user -> user.getName().equals(name));
     }
 }
