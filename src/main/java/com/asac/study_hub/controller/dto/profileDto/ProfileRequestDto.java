49b0d63658ffdfabb960b14c4478546e01c8c607
heejae369 <heejae369@gmail.com>
2024-10-27 22:56:29 +0900

    feat: ProfileRequestDto 구현

--- src/main/java/com/asac/study_hub/controller/dto/profileDto/ProfileRequestDto.java
+++ src/main/java/com/asac/study_hub/controller/dto/profileDto/ProfileRequestDto.java
@@ -1,5 +1,32 @@
 package com.asac.study_hub.controller.dto.profileDto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ProfileRequestDto {
+    Integer id;
+
+    @NotBlank
+    @JsonProperty(value = "user_name")
+    String userName;
+
+    @Email
+    String email;
+
+    @Size(min = 8, max = 20)
+    @NotBlank
+    String password;
 
 }
