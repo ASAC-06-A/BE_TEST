dcc0ff8d9b59f9ee63656db5ee4ad2ef4625cbfa
xxyeon <jykim9335@gmail.com>
2024-10-28 00:27:47 +0900

    feat: 강의 다수 삭제

--- src/main/java/com/asac/study_hub/repository/StudyIRepository.java
+++ src/main/java/com/asac/study_hub/repository/StudyIRepository.java
@@ -1,6 +1,7 @@
 package com.asac.study_hub.repository;
 
 import com.asac.study_hub.domain.Study;
+import com.asac.study_hub.domain.User;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,5 +14,7 @@ public interface StudyIRepository {
     Integer getId();
     List<Study> findByCategory(String category);
     void update(Study study, Study newstudy);
+    void deleteAll(List<Study> study);
+    Optional<Study> findByIdAndUser(Integer id, User user);
 }
 
