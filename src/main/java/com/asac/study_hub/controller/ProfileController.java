8ed7f12f8d60a358278e1f2bf029e6607920db86
heejae369 <heejae369@gmail.com>
2024-10-28 17:30:01 +0900

    refactor: getProfile 수정

--- src/main/java/com/asac/study_hub/controller/ProfileController.java
+++ src/main/java/com/asac/study_hub/controller/ProfileController.java
@@ -3,14 +3,17 @@ package com.asac.study_hub.controller;
 import com.asac.study_hub.controller.dto.common.BaseResponse;
 import com.asac.study_hub.controller.dto.common.SuccessType;
 import com.asac.study_hub.controller.dto.profileDto.ProfileResponseDto;
+import com.asac.study_hub.domain.User;
 import com.asac.study_hub.service.ProfileService;
+import com.asac.study_hub.util.SessionProvider;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,9 +27,20 @@ public class ProfileController {
     ProfileService profileService;
 
     @GetMapping("/")
-    public BaseResponse<ProfileResponseDto> getProfile(HttpServletRequest request){
-        ProfileResponseDto profileResponseDto = profileService.getMyProfile(request);
-        return BaseResponse.success(SuccessType.GETPROFILE, profileResponseDto);
+    public BaseResponse<ProfileResponseDto> getProfile(@CookieValue("session_key") Cookie cookie,
+        HttpServletRequest request) {
+        User user = SessionProvider.getValidUser(cookie.getValue(), request);
+        return BaseResponse.success(SuccessType.GET_PROFILE, ProfileResponseDto.of(user));
     }
+
+//    @PatchMapping("/{id}")
+//    public BaseResponse<ProfileResponseDto> update(@CookieValue("session_key") Cookie cookie, HttpServletRequest request, @PathVariable Integer id, @Value @RequestBody
+//        ProfileRequestDto profileRequestDto){
+//        User user =
+//        profileService.updateUser(id, profileRequestDto, user);
+//        return BaseResponse.success(SuccessType.UPDATE_PROFILE_SUCCESS, null);
+//    }
+
+    // abstract BaseResponse<ProfileResponseDto> deleteUser()
 
 }
