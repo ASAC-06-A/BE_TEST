0b00299675f20ac846679aa1b6c34aab48665899
xxyeon <jykim9335@gmail.com>
2024-10-28 20:40:34 +0900

    fix: 로그인, 회원가입 프론트 오리진 허용

--- src/main/java/com/asac/study_hub/controller/UserController.java
+++ src/main/java/com/asac/study_hub/controller/UserController.java
@@ -26,11 +26,13 @@ public class UserController {
 
     UserService userService;
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/signup")
     public BaseResponse<SignupResponseDto> signup(@RequestBody @Valid SignupRequestDto signupRequestDto) {
         return BaseResponse.success(SuccessType.SIGNUP, userService.signup(signupRequestDto));
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/signin")
     public BaseResponse<SignupResponseDto> signin(HttpServletRequest request, HttpServletResponse response, @RequestBody @Valid SignupRequestDto userDto) {
         return BaseResponse.success(SuccessType.SIGNIN, userService.signin(request, response, userDto));
