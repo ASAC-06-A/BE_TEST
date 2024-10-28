3d3c49e8bf5a47524db9df53517df8ff7f582c24
heejae369 <heejae369@gmail.com>
2024-10-28 18:50:24 +0900

    fix: Dto를 user로 바꿔주는 to() 추가

--- src/main/java/com/asac/study_hub/controller/dto/profileDto/ProfileRequestDto.java
+++ src/main/java/com/asac/study_hub/controller/dto/profileDto/ProfileRequestDto.java
@@ -1,14 +1,18 @@
 package com.asac.study_hub.controller.dto.profileDto;
 
+import com.asac.study_hub.domain.Status;
+import com.asac.study_hub.domain.User;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Getter
@@ -16,6 +20,7 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ProfileRequestDto {
+
     Integer id;
 
     @NotBlank
@@ -28,5 +33,19 @@ public class ProfileRequestDto {
     @Size(min = 8, max = 20)
     @NotBlank
     String password;
+
+    @NotNull
+    @Setter
+    Enum<Status> status;
+
+    public User to() {
+        return User.builder()
+            .id(this.id)
+            .name(this.userName)
+            .email(this.email)
+            .password(this.password)
+            .status(this.status)
+            .build();
+    }
 
 }
