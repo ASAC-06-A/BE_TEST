a0ffbc36c58dba98552a3067ed5d897906612024
heejae369 <heejae369@gmail.com>
2024-10-30 02:18:38 +0900

    feat: updateProfile 구현

--- src/main/java/com/asac/study_hub/controller/ProfileController.java
+++ src/main/java/com/asac/study_hub/controller/ProfileController.java
@@ -2,18 +2,23 @@ package com.asac.study_hub.controller;
 
 import com.asac.study_hub.controller.dto.common.BaseResponse;
 import com.asac.study_hub.controller.dto.common.SuccessType;
+import com.asac.study_hub.controller.dto.profileDto.ProfileRequestDto;
 import com.asac.study_hub.controller.dto.profileDto.ProfileResponseDto;
 import com.asac.study_hub.domain.User;
 import com.asac.study_hub.service.ProfileService;
 import com.asac.study_hub.util.SessionProvider;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,5 +47,14 @@ public class ProfileController {
 //    }
 
     // abstract BaseResponse<ProfileResponseDto> deleteUser()
+
+    @PatchMapping("/${id}")
+    public BaseResponse<ProfileResponseDto> updateProfile(@CookieValue("JSESSIONID") Cookie cookie,
+        HttpServletRequest request, @PathVariable Integer id, @Valid @RequestBody
+    ProfileRequestDto profileRequestDto) {
+        User user = SessionProvider.getValidUser(cookie.getValue(), request);
+        profileService.updateUser(user, profileRequestDto);
+        return BaseResponse.success(SuccessType.UPDATE_PROFILE_SUCCESS, null);
+    }
 
 }
