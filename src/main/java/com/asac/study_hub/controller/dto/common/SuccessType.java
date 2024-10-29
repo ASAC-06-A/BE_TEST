335609caf8c51b013b07ae217d10f0b9f48ee1b6
heejae369 <heejae369@gmail.com>
2024-10-30 01:51:11 +0900

    feat: 삭제 시 SuccessType 추가

--- src/main/java/com/asac/study_hub/controller/dto/common/SuccessType.java
+++ src/main/java/com/asac/study_hub/controller/dto/common/SuccessType.java
@@ -27,7 +27,8 @@ public enum SuccessType {
     DELETE_ALL_SUCCESS(HttpStatus.OK, "강의 다수 삭제 성공"),
     // 통합여부에 따라 삭제 예정
     UPDATE_PROFILE_SUCCESS(HttpStatus.OK, "수정에 성공하였습니다"),
-    GET_PROFILE(HttpStatus.OK, "프로필 검색 성공");
+    GET_PROFILE(HttpStatus.OK, "프로필 검색 성공"),
+    DELETE_PROFILE(HttpStatus.OK, "삭제가 완료되었습니다");
 
 
     HttpStatus status;
