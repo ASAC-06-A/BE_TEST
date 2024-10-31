1cd06fe91b0e7ab4f75b5ff784c3589f6ce7a777
heejae369 <heejae369@gmail.com>
2024-10-31 13:41:13 +0900

    fix: status 필드 삭제

--- src/main/java/com/asac/study_hub/controller/dto/profileDto/ProfileRequestDto.java
+++ src/main/java/com/asac/study_hub/controller/dto/profileDto/ProfileRequestDto.java
@@ -1,16 +1,13 @@
 package com.asac.study_hub.controller.dto.profileDto;
 
-import com.asac.study_hub.domain.Status;
 import com.asac.study_hub.domain.User;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Getter
@@ -27,10 +24,6 @@ public class ProfileRequestDto {
     @Size(min = 8, max = 20)
     @NotBlank
     String password;
-
-    @NotNull
-    @Setter
-    Enum<Status> status;
 
     public User to() {
         return User.builder()
