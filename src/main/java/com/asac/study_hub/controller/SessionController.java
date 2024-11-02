51dbeb93389f0e5c5d5745a152c1de24686c57ff
xxyeon <jykim9335@gmail.com>
2024-11-02 12:46:01 +0900

    fix: @CrossOrigin 관련 패키지 import

--- src/main/java/com/asac/study_hub/controller/SessionController.java
+++ src/main/java/com/asac/study_hub/controller/SessionController.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
