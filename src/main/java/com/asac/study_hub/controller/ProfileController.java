8727f5d9f3b03c28994c8fe5dbb8a21ffc7d1bc4
xxyeon <jykim9335@gmail.com>
2024-11-02 12:21:44 +0900

    fix: ProfileRequestDto 역직렬화 문제 해결, 로그아웃 메서드 변경, SessionProvider.remove() 수정 1. SessionProvider.remove()에서 null 처리 안해도 된다고 생각

--- src/main/java/com/asac/study_hub/controller/ProfileController.java
+++ src/main/java/com/asac/study_hub/controller/ProfileController.java
@@ -14,13 +14,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -43,7 +37,7 @@ public class ProfileController {
         HttpServletRequest request, @Valid @RequestBody ProfileRequestDto profileRequestDto) {
         User user = SessionProvider.getValidUser(cookie.getValue(), request);
         profileService.updateUser(user, profileRequestDto);
-        return BaseResponse.success(SuccessType.UPDATE_PROFILE_SUCCESS, null);
+        return BaseResponse.success(SuccessType.UPDATE_PROFILE_SUCCESS, null); //업데이터 된 정보 넘겨주는게 나은지 아니면 응답 성공 여부만 넘겨주는게 나은지
     }
 
     @DeleteMapping
@@ -54,7 +48,7 @@ public class ProfileController {
         return BaseResponse.success(SuccessType.DELETE_PROFILE, null);
     }
 
-    @DeleteMapping("/logout")
+    @PostMapping("/logout") //로그아웃은 get 또는 post로 하는데 post로 하는게 좋다고 함. 이유는 찾아보기
     public BaseResponse<Void> logoutProfile(/*@CookieValue("JSESSIONID") Cookie cookie,*/
         HttpServletRequest request) {
         SessionProvider.removeSession(/*cookie.getValue(),*/ request);
--- src/main/java/com/asac/study_hub/util/SessionProvider.java
+++ src/main/java/com/asac/study_hub/util/SessionProvider.java
@@ -25,7 +25,7 @@ public class SessionProvider {
     //로그인할때만 사용됨
     public static HttpSession createSession(HttpServletRequest request, User user) {
         HttpSession session = request.getSession(); //기존 세션이 있다면 기존꺼 사용, 없다면 새로 생성(이전에 setAttribute 해서 SESSIONID로 저장한 User객첻 없어짐)
-        if (session.getAttribute(session.getId()) == null) {
+        if (session.getAttribute(session.getId()) == null) { //null처리하는 이유는 기존에 사용한게 아니라 새로운 세션이 만들어졌을 경우 sessionId로 사용자를 저장돠어 있지 않으므로 null처리
             session.setAttribute(session.getId(), user);
         }
         return session;
@@ -39,19 +39,8 @@ public class SessionProvider {
     }
 
     public static void removeSession(/*String sessionId,*/ HttpServletRequest request) {
-        HttpSession session = getValidSession(request);
-
-//        세션이 비어있지 않다면 즉 로그인 상태라면, 세션 id에 맞는 세션 삭제 -> 로그아웃
-        if (session.getAttribute(session.getId()) != null) {
-            // 현재는 로그아웃이라 session 자체를 지우면 되기에, 쿠키에 담긴 sessionId 필요없음
-            // 여러 정보들이 session에 들어간다면 session.invalidate()
-            //session.removeAttribute(sessionId); // sessionId에 해당하는 세션에서 사용자 정보 무효화, 세션은 살아있음 ->  세션 ID는 속성이 아니므로 아무 효과 없음 세션 ID는 속성이 아니므로 아무 효과 없음
-            session.invalidate(); // 세션 완전 무효화, 멀티 로그인 구현 시, 전무 로그아웃시 사용하면 좋을듯
-        } else {// 에러
-            throw new CustomException(ExceptionType.INVALID_SESSION);
-        }
-
-
+        HttpSession session = getValidSession(request); //getValidSession에서 예외가 안터졌다함은 세션이 저장되어 있음을 의미 -> session.getAttribute 검사 안해도됨
+        session.invalidate(); // 세션 완전 무효화, 멀티 로그인 구현 시, 전무 로그아웃시 사용하면 좋을듯
     }
 }
 
