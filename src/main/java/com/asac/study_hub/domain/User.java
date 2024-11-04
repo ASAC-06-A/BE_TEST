7cec1aec1f052507e135222ba7246ad064ea2922
heejae369 <heejae369@gmail.com>
2024-11-04 16:49:53 +0900

    fix: User Class에 자기소개, 카테고리 추가

--- src/main/java/com/asac/study_hub/domain/User.java
+++ src/main/java/com/asac/study_hub/domain/User.java
@@ -19,11 +19,17 @@ public class User {
     String name;
     String email;
     String password;
+    Category profileCategory;
+    String selfIntroduction;
 
     public void update(User newUser) {
         this.name = Optional.ofNullable(newUser.getName()).orElse(this.name);
         this.email = Optional.ofNullable(newUser.getEmail()).orElse(this.email);
         this.password = Optional.ofNullable(newUser.getPassword()).orElse(this.password);
+        this.profileCategory = Optional.ofNullable(newUser.getProfileCategory())
+            .orElse(this.profileCategory);
+        this.selfIntroduction = Optional.ofNullable(newUser.getSelfIntroduction())
+            .orElse(this.selfIntroduction);
     }
 
     public void delete() {
