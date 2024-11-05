d88503e3cde842e098a014c49629ecf66e9d3f70
heejae369 <heejae369@gmail.com>
2024-11-05 14:45:36 +0900

    fix: test코드도 ProfileUpdateRequestDto로 적용

--- src/test/java/com/asac/study_hub/service/ProfileServiceTest.java
+++ src/test/java/com/asac/study_hub/service/ProfileServiceTest.java
@@ -1,16 +1,15 @@
 package com.asac.study_hub.service;
 
-import com.asac.study_hub.controller.dto.profileDto.ProfileRequestDto;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.asac.study_hub.controller.dto.profileDto.ProfileUpdateRequestDto;
 import com.asac.study_hub.domain.Status;
 import com.asac.study_hub.domain.User;
 import com.asac.study_hub.repository.UserRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class ProfileServiceTest {
@@ -26,15 +25,16 @@ class ProfileServiceTest {
         //given
 
         //업데이트할 때 email 변경 가능? -> 협의 후 진행
-        ProfileRequestDto profileRequestDto = ProfileRequestDto.builder()
-                .username("username")
-                .password("123456789")
-                .build();
+        ProfileUpdateRequestDto profileRequestDto = ProfileUpdateRequestDto.builder()
+            .username("username")
+            .password("123456789")
+            .build();
         User user = new User(40, "user", "user@gmail.com", "12345678", Status.ACTIVE);
         userRepository.save(user);
 
         //when
-        profileService.updateUser(user, profileRequestDto); //세션에 있는 user 객체를 profileRequestDto 데이터로 업데이트
+        profileService.updateUser(user,
+            profileRequestDto); //세션에 있는 user 객체를 profileRequestDto 데이터로 업데이트
 
         //then
         assertThat(profileRequestDto.getUsername()).isEqualTo(user.getName());
@@ -48,9 +48,9 @@ class ProfileServiceTest {
     @Test
     void update() {
         //given
-        ProfileRequestDto profileRequestDto = ProfileRequestDto.builder()
-                .username("username")
-                .build();
+        ProfileUpdateRequestDto profileRequestDto = ProfileUpdateRequestDto.builder()
+            .username("username")
+            .build();
         User user = new User(40, "user", "user@gmail.com", "12345678", Status.ACTIVE);
         userRepository.save(user);
 
